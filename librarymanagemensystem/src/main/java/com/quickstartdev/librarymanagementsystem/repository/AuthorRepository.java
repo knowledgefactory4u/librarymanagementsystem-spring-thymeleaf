c0c6fc81e448532a1d0e4ec7b87da9edb2c21172
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2022-03-13 12:41:20 +0530

    Delete librarymanagemensystem directory

--- librarymanagemensystem/src/main/java/com/quickstartdev/librarymanagementsystem/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quickstartdev.librarymanagementsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.quickstartdev.librarymanagementsystem.entity.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-}
--- librarymanagemensystem/src/main/java/com/quickstartdev/librarymanagementsystem/repository/PublisherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quickstartdev.librarymanagementsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.quickstartdev.librarymanagementsystem.entity.Publisher;
-
-public interface PublisherRepository extends JpaRepository<Publisher, Long> {
-
-}
